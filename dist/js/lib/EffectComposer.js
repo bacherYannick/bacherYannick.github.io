941cb3dc95db3c75a16750c95f7d4ec9751b5fec
bacherYannick <77723974+bacherYannick@users.noreply.github.com>
2021-01-25 13:49:30 +0100

    Update EffectComposer.js

--- dist/js/lib/EffectComposer.js
+++ dist/js/lib/EffectComposer.js
@@ -8,10 +8,10 @@ import {
 	Vector2,
 	WebGLRenderTarget
 } from '/three.module.js';
-import { CopyShader } from '/shaders/CopyShader.js';
-import { ShaderPass } from '/postprocessing/ShaderPass.js';
-import { MaskPass } from '/postprocessing/MaskPass.js';
-import { ClearMaskPass } from '/postprocessing/MaskPass.js';
+import { CopyShader } from '/CopyShader.js';
+import { ShaderPass } from '/ShaderPass.js';
+import { MaskPass } from '/MaskPass.js';
+import { ClearMaskPass } from '/MaskPass.js';
 
 var EffectComposer = function ( renderer, renderTarget ) {
 
